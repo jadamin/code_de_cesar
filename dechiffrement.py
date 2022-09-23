0efb2c391212a299a5becf50efe1d7130b2195c8
jadamin <jadaminpro@gmail.com>
2022-09-23 12:12:25 +0200

    code de cesar pour crypter et decrypter

---
+++ dechiffrement.py
@@ -0,0 +1,7 @@
+message_a_code = input("Entrez votre texte : ")
+cle = int(input("Entrez votre cle :"))
+message = [(ord(i)-cle) for i in message_a_code]
+message_décodé = [chr(i)for i in message]
+
+final = "".join(message_décodé)
+print(final)        
